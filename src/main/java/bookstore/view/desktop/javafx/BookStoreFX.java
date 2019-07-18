00337c0ff5b27a7bfbdbd8b6a181eb03629db989
Joan Andrés <andres6936@live.com>
2019-07-17 23:49:50 -0500

    Add BookStoreFX [Method Main]

---
+++ src/main/java/bookstore/view/desktop/javafx/BookStoreFX.java
@@ -0,0 +1,41 @@
+package bookstore.view.desktop.javafx;
+
+import bookstore.Main;
+import javafx.application.Application;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+public class BookStoreFX extends Application
+{
+
+    @Override
+    public void start( Stage stage ) throws Exception
+    {
+        Group rootGroup = new Group( new PanelCatalogBookOption() );
+
+        Scene scene = new Scene( rootGroup, 500, 500 );
+
+        stage.setTitle( "Book Store" );
+        stage.setScene( scene );
+        stage.setResizable( false );
+        stage.show();
+    }
+
+    public static void main( String[] args )
+    {
+        launch( args );
+    }
+}
